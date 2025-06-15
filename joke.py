e85dd08e44a5f3d042d4cddb9d17c160fb662674
frieda566 <frieda.schuetze@stud.leuphana.de>
2025-06-15 10:54:22 +0200

    joke.py

--- joke.py
+++ joke.py
@@ -6,3 +6,18 @@ cowsay.cow('Hello World')
 
 joke = pyjokes.get_joke()
 cowsay.cow(joke)
+
+#using the module and colorama
+from myjokes import get_funny_joke
+from colorama import init, Fore
+
+init()
+
+# printing two extra jokes - using my own module & different cowsay characters
+print(Fore.GREEN)
+cowsay.tux(get_funny_joke())
+print(Fore.RESET)
+
+print(Fore.RED)
+cowsay.dragon(get_funny_joke())
+print(Fore.RESET)
---
+++ myjokes.py
@@ -0,0 +1,4 @@
+import pyjokes
+
+def get_funny_joke():
+    return pyjokes.get_joke(category='neutral')
